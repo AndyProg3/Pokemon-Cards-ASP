0af5d8a438e98b895716470c42a0c31191e27ea6
AndyProg3 <78244781+AndyProg3@users.noreply.github.com>
2021-04-21 16:50:32 -0400

    Added files for Fight process.

---
+++ PokemonCards/Controllers/FightingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokemonCards.Controllers
+{
+    public class FightingController : Controller
+    {
+        // GET: Fighting
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
